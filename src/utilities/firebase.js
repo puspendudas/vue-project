8b413003040c2fe0027ddf45c23b540a29b1d9d1
puspendudas <49794365+puspendudas@users.noreply.github.com>
2021-09-24 20:11:17 +0530

    Update firebase.js

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -4,13 +4,13 @@ import "firebase/database";
 
 const firebaseConfig = {
     // Firebase Config
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-     
+  apiKey: "AIzaSyBe00p4W4EFMvz7pLyJpuTVDB_6xT1qzL8",
+  authDomain: "vue-login-pro.firebaseapp.com",
+  databaseURL: "https://vue-login-pro-default-rtdb.firebaseio.com",
+  projectId: "vue-login-pro",
+  storageBucket: "vue-login-pro.appspot.com",
+  messagingSenderId: "314753680679",
+  appId: "1:314753680679:web:722802dcea4202fb9c5502"
   };
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
